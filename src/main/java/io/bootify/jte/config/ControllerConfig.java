dc5345ac5fdb840bbe365114eece6b02fcfc99c9
Bootify <222237037+bootify-io@users.noreply.github.com>
2026-02-02 09:18:40 +0000

    spring boot update 4.0.2

--- src/main/java/io/bootify/jte/config/ControllerConfig.java
+++ src/main/java/io/bootify/jte/config/ControllerConfig.java
@@ -1,6 +1,9 @@
 package io.bootify.jte.config;
 
+import gg.jte.springframework.boot.autoconfigure.JteViewResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.core.Ordered;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -12,6 +15,11 @@ public class ControllerConfig {
     @InitBinder
     public void initBinder(final WebDataBinder binder) {
         binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+    @Autowired
+    public void jteViewResolverOrder(final JteViewResolver jteViewResolver) {
+        jteViewResolver.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 
 }
